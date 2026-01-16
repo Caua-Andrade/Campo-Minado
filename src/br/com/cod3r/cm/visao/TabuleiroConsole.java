b32e45f94f52ae0f9f3082180a1e24b2f8c81606
Caua-Andrade <cauaandsilva@gmail.com>
2026-01-16 08:42:07 -0300

    Troquei a ordem das coordenadas

--- src/br/com/cod3r/cm/visao/TabuleiroConsole.java
+++ src/br/com/cod3r/cm/visao/TabuleiroConsole.java
@@ -50,7 +50,7 @@ public class TabuleiroConsole {
             while (!tabuleiro.objetivoAlcancado()) {
                 System.out.println(tabuleiro);
 
-                String digitado = capturarValorDigitado("Digite (x, y): ");
+                String digitado = capturarValorDigitado("Digite (y, x): ");
 
                 Iterator<Integer> xy = Arrays.stream(digitado.split(","))
                         .map(e -> Integer.parseInt(e.trim())).iterator();
